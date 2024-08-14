cadea399bdcd9ee60627b12cb3348bba567bf896
Kirito-kun228 <andreevpv76@gmail.com>
2024-08-15 00:35:57 +0300

    init_base_func

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-print('hello world')
+import telebot
+import schedule
+import os
+import time
+import json
+
+token = os.environ.get('TOKEN')
+
+bot = telebot.TeleBot(token)
+
+
+# функция получения погоды
+def weather():
+    pass
+
+
+# функция получения гороскопа
+def horoscope():
+    pass
+
+
+# функция получения новостей
+def news():
+    pass
+
+
+def main():
+    print('1')
+
+
+schedule.every().day.at('09:00').do(main)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
